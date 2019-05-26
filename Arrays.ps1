c833972ce35eb66eac7075fc068d900663afa2bc
Turtleman1024 <kmurry6262@gmail.com>
2019-05-25 23:15:48 -0700

    Adding Arrays

---
+++ Arrays.ps1
@@ -0,0 +1,35 @@
+﻿# Arrays
+
+# Declaring and accessing elements
+$array = "Turtle", "man"
+$array[0]
+$array[1]
+$array
+
+
+$array.GetType()
+
+# Updating Arrays
+$array = "Turtle", "boy"
+$array
+
+$array[0] = "Fish"
+$array
+
+# Formal Array Declaration Syntax
+$array = @("Steve", "Rogers")
+$array
+
+# Have to using Formal Syntax to create a empty array
+$array = @()
+$array.Count()
+
+# Can load arrays using numeric range notation
+$array = 1..5
+$array
+
+# Check to see if an item exists
+$numbers = 2, 56, 1024
+$numbers -contains 56
+$numbers -notcontains 128
+$numbers -notcontains 56
